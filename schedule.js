1e1cb875d6f01558ae125257ce5c9293c420b4a5
HavatzalotPnim <38509422+HavatzalotPnim@users.noreply.github.com>
2018-10-09 20:32:08 +0300

    Update schedule.js

--- schedule.js
+++ schedule.js
@@ -4,11 +4,11 @@ function gradeWeek(sun, mon, tue, wed, thu)
 }
 
 var gradeWeek3 = new gradeWeek(
-	[[], ["math", "econ"], ["arab"], [], ["math", "comp", "philo"], [],	["philo", "math", "comp", "econ"]],
-	[["econ", "comp", "philo"], ["econ", "comp"], [], [], [], [],	["philo", "math", "comp", "econ"]],
-	[[], [], ["math", "econ", "comp"], ["econ", "comp"], [], ["kamat"],	["philo", "math", "comp", "econ"]],
-	[["philo", "math"], ["math"], ["econ", "math"], ["math", "comp", "econ"], [], ["philo", "math", "comp", "econ"],	["philo", "math", "comp", "econ"]],
-	[["math", "econ"], [], ["math", "comp", "philo"], [], [], [],	["philo", "math", "comp", "econ"]]
+	[[], ["math", "econ", "philo"], ["arab"], [], ["comp", "philo", "econ"], ["math", "econ", "philo", "comp"],	["philo", "math", "comp", "econ"]],
+	[[], [], [], ["econ"], ["econ", "comp"], ["math", "econ", "philo"],	["philo", "math", "comp", "econ"]],
+	[["econ", "comp", "philo"], ["econ", "comp", "philo"], ["arab"], ["econ"], [], ["kamat"],	["philo", "math", "comp", "econ"]],
+	[[], ["math", "comp", "philo"], ["comp"], [], [], ["philo", "math", "comp", "econ"],	["philo", "math", "comp", "econ"]],
+	[["math", "philo"], ["math", "philo", "comp"], ["math", "econ"], ["math", "econ"], [], [],	["philo", "math", "comp", "econ"]]
 );
 var gradeWeek2 = new gradeWeek(
 	[[], [], ["arab"], ["comp", "econ"], [], [],	["philo", "math", "comp", "econ"]],
@@ -18,9 +18,9 @@ var gradeWeek2 = new gradeWeek(
 	[["math", "econ", "comp"], ["econ"], ["math", "philo", "comp"], ["comp", "philo", "econ"], [], [],	["philo", "math", "comp", "econ"]]
 );
 var gradeWeek1 = new gradeWeek(
-	[[], ["arab"], [], ["math", "econ"], [], ["math", "philo"],	["philo", "math", "comp", "econ"]],
-	[["philo"], ["philo"], ["math", "econ"], ["comp"], [], [],	["philo", "math", "comp", "econ"]],
-	[["math", "philo", "econ"], ["philo"], [], ["math", "philo"], [], [],	["philo", "math", "comp", "econ"]],
-	[[], [], [], ["math"], [], ["philo", "math", "comp", "econ"],	["philo", "math", "comp", "econ"]],
-	[["econ"], ["math", "econ", "philo"], [], ["philo", "math", "comp", "econ"], ["philo", "math", "comp", "econ"], ["philo", "math", "comp", "econ"],	["philo", "math", "comp", "econ"]]
+	[[], ["arab"], [], ["comp"], [], [],	["philo", "math", "comp", "econ"]],
+	[["econ"], ["econ"], [], ["comp"], [], [],	["philo", "math", "comp", "econ"]],
+	[["arab"], [], ["philo"], ["philo"], [], [],	["philo", "math", "comp", "econ"]],
+	[[], ["comp"], [], [], [], ["philo", "math", "comp", "econ"],	["philo", "math", "comp", "econ"]],
+	[["comp"], [], [], ["econ"], ["philo", "math", "comp", "econ"], ["philo", "math", "comp", "econ"],	["philo", "math", "comp", "econ"]]
 );
