5a242850f2e994e22fb6804d2038ce5e97787d06
HavatzalotPnim <38509422+HavatzalotPnim@users.noreply.github.com>
2018-10-12 23:18:39 +0300

    Update schedule.js

--- schedule.js
+++ schedule.js
@@ -4,11 +4,11 @@ function gradeWeek(sun, mon, tue, wed, thu)
 }
 
 var gradeWeek3 = new gradeWeek(
-	[[], ["math", "econ", "philo"], ["arab"], ["math", "econ", "philo", "comp"], ["comp", "philo", "econ"], ["math", "econ", "philo", "comp"],	["philo", "math", "comp", "econ"]],
+	[[], ["math", "econ", "philo"], ["arab"], ["math", "econ", "philo", "comp"], ["comp", "philo", "econ"], ["math", "econ", "philo", "comp"], ["philo", "math", "comp", "econ"]],
 	[["math", "econ", "philo", "comp"], ["math", "econ", "philo", "comp"], ["math", "econ", "philo", "comp"], ["econ"], ["econ", "comp"], ["math", "econ", "philo"],	["philo", "math", "comp", "econ"]],
 	[["econ", "comp", "philo"], ["econ", "comp", "philo"], ["arab"], ["econ"], ["math", "econ", "philo", "comp"], ["kamat"],	["philo", "math", "comp", "econ"]],
 	[["math", "econ", "philo", "comp"], ["math", "comp", "philo"], ["comp"], ["math", "econ", "philo", "comp"], ["math", "econ", "philo", "comp"], ["philo", "math", "comp", "econ"],	["philo", "math", "comp", "econ"]],
-	[["math", "philo"], ["math", "philo", "comp"], ["math", "econ"], ["math", "econ"], ["math", "econ", "philo", "comp"], ["math", "econ", "philo", "comp"],	["philo", "math", "comp", "econ"]]
+	[["math", "philo"], ["math", "philo", "comp"], ["math", "econ"], ["math", "econ"], ["math", "econ", "philo", "comp"], ["math", "econ", "philo", "comp"], ["philo", "math", "comp", "econ"]]
 );
 var gradeWeek2 = new gradeWeek(
 	[[], [], ["arab"], ["comp", "econ"], [], [],	["philo", "math", "comp", "econ"]],
